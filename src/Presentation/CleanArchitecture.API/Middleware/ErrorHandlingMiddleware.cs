bdfbe84d99de5f349d97f28606b8527cf270f415
Jacky Nguyen <info.hieuit@gmail.com>
2025-04-27 17:59:23 +0700

    remove file not using

---
+++ src/Presentation/CleanArchitecture.API/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace CleanArchitecture.API.Middleware;
+
+public class ErrorHandlingMiddleware
+{
+
+}
--- src/Presentation/CleanArchitecture.API/Program.cs
+++ src/Presentation/CleanArchitecture.API/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddEndpointsApiExplorer();
 // Add DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 }
     );
 
